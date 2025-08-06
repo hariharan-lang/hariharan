843649681194140c47baf73f62f050f8a788a48b
Hari <Admin@DESKTOP-AU0LG9Q>
2025-08-06 12:49:37 +0530

    " trial git"

---
+++ Firstclass/src/main/java/com/Firstclass/FirstclassApplication.java
@@ -0,0 +1,13 @@
+package com.Firstclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstclassApplication.class, args);
+	}
+
+}
